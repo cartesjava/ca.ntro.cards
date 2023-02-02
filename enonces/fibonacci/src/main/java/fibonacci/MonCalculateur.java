2114f1adcc57c8a7eab6567d0d101467707cca16
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2023-02-02 10:47:25 -0500

    màj énoncés

--- enonces/fibonacci/src/main/java/fibonacci/MonCalculateur.java
+++
@@ -1,7 +0,0 @@
-package fibonacci;
-
-import ca.ntro.cards.fibonacci.models.Calculateur;
-
-public class MonCalculateur extends Calculateur {
-
-}
--- enonces/fibonacci/src/main/java/fibonacci/MonFibonacci.java
+++
@@ -1,7 +0,0 @@
-package fibonacci;
-
-import ca.ntro.cards.fibonacci.models.Fibonacci;
-
-public class MonFibonacci extends Fibonacci {
-
-}
